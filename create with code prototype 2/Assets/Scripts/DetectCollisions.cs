0bef71fb287e8c2bb43b205fcc2f51d820ca8a6f
Michael Kline <klinem@gmail.com>
2020-08-07 22:42:16 -0600

    week 2 added

---
+++ create with code prototype 2/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Destroy(gameObject); *old before slider*
+        //Destroy(other.gameObject); *old before slider*
+
+        if (other.CompareTag("Animal"))
+        {
+            other.GetComponent<AnimalHunger>().FeedAnimal(1);
+            other.GetComponent<Transform>().localScale = new Vector3(3, 3, 3);
+            Destroy(gameObject);
+        }
+    }
+}
