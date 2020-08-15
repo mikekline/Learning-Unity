64155791f0fc0d2bcb6a35d60682851caf29fe07
Michael Kline <klinem@gmail.com>
2020-08-15 05:09:30 -0600

    week 4 added

---
+++ create with code prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 110.0f;
+    private float zBound = 7;
+    private Rigidbody playerRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        BlockPlayerPosition();
+    }
+    //Moves the player based on arrow key input
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float jumpInput = Input.GetAxis("Jump");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+        playerRb.AddForce(Vector3.up * speed * jumpInput);
+    }
+
+    //Prevent the player from leaving the top or bottom of the screen
+    void BlockPlayerPosition()
+    {
+          if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    } 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("bad guy"))
+        {
+            Debug.Log("player is drunk, Go home!");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("power up"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
---
+++ create with code prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] badGuys;
+    public GameObject powerup;
+    private float zBadGuySpawn = 12.0f;
+    private float XSpawnRange = 16.0f;
+    private float zpowerupRange = 16.0f;
+    private float ySpawn = 0.75f;
+
+    private float powerupSpawnTime = 5.0f;
+    private float badGuySpawnTime = 1.0f;
+    private float startDelay = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SpawnRandomBadGuy();
+        //SpawnPowerup();
+        InvokeRepeating("SpawnRandomBadGuy", startDelay, badGuySpawnTime);
+        InvokeRepeating("SpawnPowerup", startDelay, powerupSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnRandomBadGuy()
+    {
+        float randomX = Random.Range(-XSpawnRange, XSpawnRange);
+        int randomIndex = Random.Range(0, badGuys.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, zBadGuySpawn);
+        Instantiate(badGuys[randomIndex], spawnPos, badGuys[randomIndex].gameObject.transform.rotation);
+    }
+
+    void SpawnPowerup()
+    {
+        float randomX = Random.Range(-XSpawnRange, XSpawnRange);
+        float randomZ = Random.Range(-zpowerupRange, zpowerupRange);
+
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, randomZ);
+        Instantiate(powerup, spawnPos, powerup.gameObject.transform.rotation);
+    }
+}
